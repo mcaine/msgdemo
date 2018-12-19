cd54fa96a8a5c6a2b12be68232588804a5d9b4d2
Mike Caine <mikey.caine@btinternet.com>
2018-12-19 14:40:48 +0000

    Tidy up includes

--- src/main/java/com/mikeycaine/msgdemo/service/MessageService.java
+++ src/main/java/com/mikeycaine/msgdemo/service/MessageService.java
@@ -1,12 +1,8 @@
 package com.mikeycaine.msgdemo.service;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
